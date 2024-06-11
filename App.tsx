2e5259302d725a46b938bd8b179f91fe8c2d2dc9
Felipe Faustini <felipefaustini@MacBook-Pro-2.local>
2024-06-11 07:59:30 -0300

    feat(initial setup): added src and assets

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor={'transparent'}
+        translucent
+      />
     </View>
   );
 }
@@ -13,8 +27,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#202024',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: 'Roboto_700Bold',
+  }
 });

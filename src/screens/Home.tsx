2a4238c2f502879bcf6238aa268dbb9a0cd7da72
Felipe Faustini <felipefaustini@MacBook-Pro-2.local>
2024-06-19 15:46:05 -0300

    feat: exercise screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { HStack, VStack, FlatList, Heading, Text } from "native-base";
+import { useNavigation } from "@react-navigation/native";
 
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
 import { ExerciseCard } from "@components/ExerciseCard";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
 export function Home() {
   const [groups, setGroups] = useState([
@@ -14,8 +16,25 @@ export function Home() {
     "glúteos",
     "panturrilhas",
   ]);
-  const [exercises, setExercices] = useState(["remada", "abdominais", '1', '2', '3', '4', '5', '6', '7', '8']);
+  const [exercises, setExercices] = useState([
+    "remada",
+    "abdominais",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+  ]);
   const [groupSelected, setGroupSelected] = useState("costas");
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate('exercise');
+  }
 
   return (
     <VStack flex={1}>
@@ -27,7 +46,9 @@ export function Home() {
         renderItem={({ item }) => (
           <Group
             name={item}
-            isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
+            isActive={
+              groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()
+            }
             onPress={() => setGroupSelected(item)}
           />
         )}
@@ -54,7 +75,9 @@ export function Home() {
         <FlatList
           data={exercises}
           keyExtractor={(item) => item}
-          renderItem={({ item }) => <ExerciseCard />}
+          renderItem={({ item }) => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          )}
           showsVerticalScrollIndicator={false}
           _contentContainerStyle={{ paddingBottom: 20 }}
         />

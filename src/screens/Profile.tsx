8f35ea050eb264a0e41db9a65a5141c2a34115d7
Felipe Faustini <felipefaustini@MacBook-Pro-2.local>
2024-06-22 12:45:19 -0300

    feat: allowing user to select photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
 import {
   Center,
   ScrollView,
@@ -7,7 +7,10 @@ import {
   Skeleton,
   Text,
   Heading,
+  useToast
 } from "native-base";
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
@@ -16,8 +19,54 @@ import { Button } from "@components/Button";
 
 const PHOTO_SIZE = 33;
 
+interface IPhotoInfo {
+  exists: boolean;
+  isDirectory: boolean;
+  md5: string;
+  modificationTime: number;
+  size: number;
+  uri: string;
+}
+
 export function Profile() {
   const [isPhotoLoading, setIsPhotoLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://picsum.photos/200');
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelection() {
+    setIsPhotoLoading(true);
+    try {
+      const selectedPhoto = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+        base64: false,
+      });
+  
+      if (selectedPhoto.canceled) {
+        return;
+      }
+
+      const photoUri = selectedPhoto.assets[0].uri;
+      if (photoUri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoUri) as IPhotoInfo;
+        if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+          return toast.show({
+            title: 'Essa imagem é muito grande. A imagem deve ter no máximo 5MB.',
+            placement: 'top',
+            bgColor: 'red.500',
+          });
+        }
+        setUserPhoto(selectedPhoto.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsPhotoLoading(false);
+    }
+  }
 
   return (
     <VStack flex={1}>
@@ -35,7 +84,7 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{ uri: "https://picsum.photos/200" }}
+              source={{ uri: userPhoto }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
@@ -48,6 +97,7 @@ export function Profile() {
               fontSize={"md"}
               mt={2}
               mb={8}
+              onPress={handleUserPhotoSelection}
             >
               Alterar foto
             </Text>

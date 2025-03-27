d22b645d136ca931d2769f4ebc301b5401f2b891
Felipe Faustini <felipefaustini@MacBook-Pro-2.local>
2025-03-27 07:50:51 -0300

    feat: galeria de fotos

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,25 +1,69 @@
+import { useState } from "react";
 import { ScrollView, TouchableOpacity } from "react-native";
-import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
+import { Center, Heading, Text, useToast, VStack } from "@gluestack-ui/themed";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { ToastMessage } from "@components/ToastMessage";
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState("https://github.com/fabfdev.png");
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: "images",
+        quality: 1,
+        aspect: [4, 4],
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      const photoUri = photoSelected.assets[0].uri;
+
+      if (photoUri) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoUri)) as {
+          size: number;
+        };
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 1) {
+          return toast.show({
+            placement: "top",
+            render: ({ id }) => (
+              <ToastMessage
+                id={id}
+                action="error"
+                title="Imagem inválida"
+                description="Essa imagem é muito grande. Escolha uma de até 5MB"
+                onClose={() => toast.close(id)}
+              />
+            ),
+          });
+        }
+
+        setUserPhoto(photoUri);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
 
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt={"$6"} px={"$10"}>
-          <UserPhoto
-            source={{ uri: "https://github.com/fabfdev.png" }}
-            alt="Foto"
-            size="xl"
-          />
+          <UserPhoto source={{ uri: userPhoto }} alt="Foto" size="xl" />
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="$green500"
               fontFamily="$heading"
